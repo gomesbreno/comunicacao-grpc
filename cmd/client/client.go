7f9cc067f64396168b086f981f4248e099ec1e2c
gomesbreno <brenodmelogomes@outlook.com>
2021-05-26 16:52:44 -0300

    Comunicação usando streaming

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"time"
 
 	"github.com/codeedu/fc2-grpc/pb/pb"
 	"google.golang.org/grpc"
@@ -17,7 +19,10 @@ func main() {
 	defer connection.Close()
 
 	client := pb.NewUserServiceClient(connection)
-	AddUser(client)
+	// AddUser(client)
+	// AddUserVerbose(client)
+	AddUsers(client)
+
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -32,4 +37,76 @@ func AddUser(client pb.UserServiceClient) {
 		log.Fatalf("Could not make gRPC request: %v", err)
 	}
 	fmt.Println(res)
+}
+
+func AddUserVerbose(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "João",
+		Email: "joao@email.com",
+	}
+	responseStream, err := client.AddUserVerbose(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	for {
+		stream, err := responseStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Could not receive the msg: %v", err)
+		}
+		fmt.Println("status: ", stream.Status, " - ", stream.GetUser())
+	}
+
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "w1",
+			Name:  "Wesley",
+			Email: "wes@email.com",
+		},
+		&pb.User{
+			Id:    "w2",
+			Name:  "Wesley 2",
+			Email: "wes2@email.com",
+		},
+		&pb.User{
+			Id:    "w3",
+			Name:  "Wesley 3",
+			Email: "wes3@email.com",
+		},
+		&pb.User{
+			Id:    "w4",
+			Name:  "Wesley 4",
+			Email: "we4s@email.com",
+		},
+		&pb.User{
+			Id:    "w5",
+			Name:  "Wesley 5",
+			Email: "we5s@email.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error receiving response: %v", err)
+	}
+
+	fmt.Println(res)
+
 }
--- services/user.go
+++ services/user.go
@@ -3,6 +3,9 @@ package services
 import (
 	"context"
 	"fmt"
+	"io"
+	"log"
+	"time"
 
 	"github.com/codeedu/fc2-grpc/pb/pb"
 )
@@ -10,7 +13,9 @@ import (
 /*
 type UserServiceServer interface {
 	AddUser(context.Context, *User) (*User, error)
+	AddUserVerbose(*User, UserService_AddUserVerboseServer) error
 	mustEmbedUnimplementedUserServiceServer()
+	AddUsers(ctx context.Context, opts ...grpc.CallOption) (UserService_AddUsersClient, error)
 }
 */
 
@@ -32,4 +37,62 @@ func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error)
 		Name:  req.GetName(),
 		Email: req.GetEmail(),
 	}, nil
+}
+
+func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+	stream.Send(&pb.UserResultStream{
+		Status: "Init",
+		User:   &pb.User{},
+	})
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Inserting",
+		User:   &pb.User{},
+	})
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "User has been inserted",
+		User: &pb.User{
+			Id:    "123",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Completed",
+		User: &pb.User{
+			Id:    "123",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+	time.Sleep(time.Second * 3)
+
+	return nil
+}
+
+func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
+	users := []*pb.User{}
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.Users{
+				User: users,
+			})
+		}
+		if err != nil {
+			log.Fatalf("Error receiving stream: %v", err)
+		}
+		users = append(users, &pb.User{
+			Id:    req.GetId(),
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		})
+		fmt.Println("adding: ", req.GetName())
+	}
 }
